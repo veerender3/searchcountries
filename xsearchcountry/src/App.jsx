f88f7ff2a72797b9f3cfd127496567a436f6782b
Veerender <veerender1803@gmail.com>
2024-07-17 16:24:43 +0530

    second commit

---
+++ xsearchcountry/src/App.jsx
@@ -0,0 +1,12 @@
+import Xsearchcountry from "./components/Xsearchcountry"
+
+
+const App = () => {
+  return (
+    <div>
+      <Xsearchcountry/>
+    </div>
+  )
+}
+
+export default App
---
+++ xsearchcountry/src/components/Xsearchcountry.jsx
@@ -0,0 +1,71 @@
+import {useEffect, useState } from "react";
+import styles from './Xsearchcountry.module.css';
+const Xsearchcountry = () => {
+    const [country, setCountry] = useState([]);
+    const [countrySearch, setCountrySearch] = useState('');
+     const [debounceSearch, setDebounceSearch] = useState('');
+   
+    
+      // getting All country data in mounting phase
+  const handleSearchBarApi = async() =>{  
+        try{
+            const response = await fetch(`https://restcountries.com/v3.1/all`);
+            const responseData = await response.json();
+              // console.log('getting datat', responseData)
+             setCountry(responseData);
+        }
+        catch(error){
+          
+              console.error('something went wrong', error.message)
+             }
+    }
+    useEffect(()=>{
+      handleSearchBarApi();
+    },[])
+    
+    
+    useEffect(() => {
+         
+        const DebounceApi = setTimeout(() => {
+          console.log('debouncing search', DebounceApi)
+          
+            setDebounceSearch(countrySearch);
+           
+        }, 500);
+          return () => clearTimeout(DebounceApi)
+        }, [countrySearch]);
+
+
+   
+   const filtersearchquery = country.filter(searchName =>
+    searchName.name.common.toLowerCase().includes(debounceSearch.toLowerCase())
+  ) 
+ return (
+    <div className={styles.searchContainer}>
+     <input 
+     type="text" 
+      className={styles.searchBox}  
+      value = {countrySearch} 
+      placeholder="Search for countries..."
+      onChange={(e) => setCountrySearch(e.target.value)}
+      />
+      <div className={styles.container}>
+      {console.log('county', styles.countryCard)}
+        {filtersearchquery.map(data =>(
+        // console.log('filtersearchquery',filtersearchquery)
+        //  console.log('county', styles.countryCard)
+          <div className={styles.countryCard} key={data.cca3}>
+            <img 
+                className={styles.imgSrc} 
+                src={data.flags.png} 
+                alt={data.cca3}
+                />
+                <h3>{data.name.common}</h3> 
+                </div>
+                ))}
+         </div>
+     </div>
+  );
+}
+
+export default Xsearchcountry
